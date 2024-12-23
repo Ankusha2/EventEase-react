223a405f9a62e5a158b0cc316c91d83abb67743b
Ankusha2 <ankusha8427@gmail.com>
2024-12-23 20:10:54 +0530

    Update Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -6,9 +6,9 @@ export default function Contact() {
     <div id='contact'>
           <form>
             <h2><center>Contact Us</center></h2> <br />
-            <input type="text" className="form-control" placeholder="Enter the Name"/><br/>
-            <input type="text" className="form-control" placeholder="Enter the Email"/><br/>
-            <input type="text" className="form-control" placeholder="Enter the PhoneNo."/><br/>
+            <input type="text" className="form-control" placeholder="Enter the Name" required /><br/>
+            <input type="text" className="form-control" placeholder="Enter the Email" required /><br/>
+            <input type="text" className="form-control" placeholder="Enter the PhoneNo." required /><br/>
             <textarea rows="3"  className="input-box" placeholder="Enter the Message"></textarea><br/><br/>
             <button type="submit" className="btn">Submit</button>
            </form>
